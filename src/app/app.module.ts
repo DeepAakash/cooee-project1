0b29aeba40a3c918b8343a974193c051a99e18fa
Aakash Deep <aakashsinghkarnaal@gmail.com>
2024-03-10 18:41:55 +0530

    Add necessary folders and dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './MyComponents/home/home.component';
+import { LoginComponent } from './MyComponents/login/login.component';
+import { CreateComponent } from './MyComponents/create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
