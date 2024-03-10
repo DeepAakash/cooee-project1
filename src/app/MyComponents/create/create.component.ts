50f2525bcb134df5a9f0d4907d752ed172f1c747
Aakash Deep <aakashsinghkarnaal@gmail.com>
2024-03-11 05:11:03 +0530

    Change Custom theme, Make toolbar sticky

--- src/app/MyComponents/create/create.component.ts
+++ src/app/MyComponents/create/create.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+interface Item {
+  id: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent {
+  device!: string;
+  name!: string;
+  occurred!: Date;
+  item: Item = {
+    id: '',
+    name: ''
+  };
 
+  create() {
+    // Your create function logic here
+  }
 }
