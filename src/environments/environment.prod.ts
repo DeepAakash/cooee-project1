8ac84693231aebc8a39ef40bf296e44724b9d626
Aakash Deep <aakashsinghkarnaal@gmail.com>
2024-03-13 09:53:12 +0530

    Fixing CORS issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
+// Route when production is done 
 export const environment = {
     production: true,
-    apiUrl: 'https://cooee-project1-api.vercel.app/api'
+    apiUrl: 'https://cooee-project1-api.vercel.app/api',
   };
   
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
+// Route while developing
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api'
+    apiUrl: 'http://localhost:3000/api',
   };
   
