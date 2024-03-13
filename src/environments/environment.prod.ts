d2942363e21d53f399ef94110e5ac54050e11496
Aakash Deep <aakashsinghkarnaal@gmail.com>
2024-03-13 21:56:27 +0530

    CORS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
-// Route when production is done 
 export const environment = {
     production: true,
-    apiUrl: 'https://cooee-project1-api.vercel.app/api',
+    apiUrl: 'https://cooee-project1-api.vercel.app/api'
   };
   
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
-// Route while developing
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api',
+    apiUrl: 'http://localhost:3000/api'
   };
   
